bb7cb2918f3d3bc98889d0169330c457dfca40a2
mbottini <mbottini1@gmail.com>
2015-08-10 15:25:38 -0700

    Added comments and changed = overload to skill.cpp
    More comments in certain sections so that the professor doesn't get cranky.
    
    Skill = operator just returns *this if the two objects are the same.

--- skill.cpp
+++ skill.cpp
@@ -7,6 +7,8 @@ Skill::Skill(const char* name, const char* description, const int& level) {
     setValues(name, description, level);
     return;
 }
+
+// Default constructor. Defaults to "", "", 0 when called with no args.
 void Skill::setValues(const char* name, const char* description,
                       const int& level) {
     _name = new char[strlen(name) + 1];
@@ -20,14 +22,14 @@ void Skill::setValues(const char* name, const char* description,
             _level = level;
         }
 
-        else {
+        else { // Description alloc failed, so we delete name as well.
             _level = 0;
             delete [] _name;
             _name = NULL;
         }
     }
 
-    else {
+    else { // Name is already NULL from failed alloc.
         _description = NULL;
         _level = 0;
     }
@@ -38,17 +40,19 @@ void Skill::setValues(const char* name, const char* description,
 Skill::Skill(const Skill& originalSkill) {
     _name = NULL;
     _description = NULL;
-    *this = originalSkill;
+    *this = originalSkill; // See below for overloaded = operator.
     return;
 }
 
 Skill& Skill::operator =(const Skill& originalSkill) {
-    delete [] _name;
-    delete [] _description;
-    _level = 0;
+    if(_name != originalSkill.getName()) { // Don't delete if they're the same.
+        delete [] _name;
+        delete [] _description;
+        _level = 0;
 
-    setValues(originalSkill.getName(), originalSkill.getDescription(),
-              originalSkill.getLevel());
+        setValues(originalSkill.getName(), originalSkill.getDescription(),
+                  originalSkill.getLevel());
+    }
 
     return *this;
 }
@@ -72,6 +76,7 @@ int Skill::getLevel() const {
     return _level;
 }
 
+// I prefer overloaded operators, so this is just a wrapper for the assignment.
 std::ostream& Skill::Display(std::ostream& os) {
     os << *this;
     return os;
